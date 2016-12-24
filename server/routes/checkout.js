0724770b8041c6e3a239df9497901963c9022902
Paul-Kijtapart <scc385@gmail.com>
2016-12-24 05:27:47 -0800

    when user click checkout both product and cart on server will be updated

--- server/routes/checkout.js
+++ server/routes/checkout.js
@@ -16,32 +16,84 @@ router.use(jsonParser);
 // Database
 var mongoose = require('mongoose');
 var Order = mongoose.model('Order');
+var Product = mongoose.model('Product');
+
+// "{"Box1": 10}"
 
 /**
-1.	accept a JSON formatted object (cart) and 
-	total as parameters that you will need to store in your orders table.
-
 2. also update your products table (Deduct the value in the products table according to cart) 
 3. Update clients on Updated products and re-set the cart.
 */
-router.post('/', function(req, res, next) {
-	var cart = req.body;
-	Order.update({
-		_id: "1"
-	}, {
-		$set: {
-			cart: cart.cart,
-			total: cart.total
+
+// Return true if an object is empty {}
+function isEmpty(obj) {
+	for (let prop in obj) {
+		if (Object.prototype.hasOwnProperty.call(obj, prop)) {
+			return false;
 		}
-	}, {
-		upsert: true
-	}, function(err, updateStatus) {
+	}
+	return true;
+};
+
+// Send back the singleton order in Order database
+router.get('/', function(req, res, next) {
+	Order.find({}, function(err, order) {
 		if (err) {
 			console.error(err);
 		}
 		res.status(200);
-		res.send(updateStatus);
+		res.json(order);
 	});
+});
+
+// Update orders and products collections on user's checkout
+router.post('/', function(req, res, next) {
+	var cart = req.body;
+	if (isEmpty(cart)) {
+		res.json({
+			err: "Please attach a cart object"
+		});
+	} else {
+		// Update the order collection
+		Order.update({
+			_id: "1"
+		}, {
+			$set: {
+				cart: cart.cart,
+				total: cart.total
+			}
+		}, {
+			upsert: true
+		}, function(err, updateStatus) {
+			if (err) {
+				console.error(err);
+			}
+			res.status(200);
+			res.send(updateStatus);
+		});
+
+		// Update the product collection
+		// console.log(cart);
+		var cart = JSON.parse(cart.cart);
+		for (let productName in cart) {
+			Product.update({
+				name: productName,
+				quantity: {
+					$gte: cart[productName]
+				}
+			}, {
+				$inc: {
+					quantity: -cart[productName]
+				}
+			}, function(err, updateStatus) {
+				if (err) {
+					console.error("Failed at " + productName);
+					console.error(err);
+				}
+				console.log(updateStatus);
+			});
+		}
+	}
 });
 
 module.exports = router;
